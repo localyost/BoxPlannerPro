7bad637db47d0c5995b4a07bbec59effd505259e
p.yost <peter.yost@intive.com>
2023-08-25 16:29:54 +0200

    add activity_main.xml

--- app/src/main/java/com/localyost/boxplannerpro/MainActivity.kt
+++ app/src/main/java/com/localyost/boxplannerpro/MainActivity.kt
@@ -15,32 +15,6 @@ import com.localyost.boxplannerpro.ui.theme.BoxPlannerProTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BoxPlannerProTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BoxPlannerProTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
