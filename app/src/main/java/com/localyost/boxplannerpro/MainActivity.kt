110512587698b630f542a623bac237f6eb51f159
p.yost <peter.yost@intive.com>
2023-08-25 17:05:50 +0200

    add preferences

--- app/src/main/java/com/localyost/boxplannerpro/MainActivity.kt
+++ app/src/main/java/com/localyost/boxplannerpro/MainActivity.kt
@@ -2,19 +2,15 @@ package com.localyost.boxplannerpro
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.localyost.boxplannerpro.ui.theme.BoxPlannerProTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE)
+        sharedPreferences.edit().putBoolean("test", false).apply();
+        val pref = sharedPreferences.getBoolean("test", false);
+        println(pref)
     }
 }
