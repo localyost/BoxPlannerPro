e2876483db68bf9b78c5448d1870db56bbf4d91a
p.yost <peter.yost@intive.com>
2023-09-07 21:55:52 +0200

    get classes

--- app/src/main/java/com/localyost/boxplannerpro/CalenderActivity.kt
+++ app/src/main/java/com/localyost/boxplannerpro/CalenderActivity.kt
@@ -2,11 +2,33 @@ package com.localyost.boxplannerpro
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.localyost.boxplannerpro.constants.SettingStrings
+import com.localyost.boxplannerpro.remote.ApiClient
+import com.localyost.boxplannerpro.remote.ApiService
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CalenderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calender)
+
+        val apiInterface = ApiClient.getClient().create(ApiService::class.java)
+        val sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE)
+        val queryParams = mapOf("date" to "2023-09-20", "boxId" to "d37974e3-d6c0-4470-ae02-41d1b0c0ca86")
+        val call = apiInterface.getClasses(sharedPreferences.getString(SettingStrings.TOKEN, ""), queryParams)
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                println()
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
--- app/src/main/java/com/localyost/boxplannerpro/remote/ApiService.java
+++ app/src/main/java/com/localyost/boxplannerpro/remote/ApiService.java
@@ -1,11 +1,14 @@
 package com.localyost.boxplannerpro.remote;
 
+import java.util.Map;
+
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.QueryMap;
 
 public interface ApiService {
 
@@ -14,4 +17,7 @@ public interface ApiService {
 
     @POST("Account/Login")
     Call<ResponseBody> login(@Body LoginBody body);
+
+    @GET("breeze/SpecificScheduleBreeze/GetSchedulesForMonth")
+    Call<ResponseBody> getClasses(@Header("Cookie") String authToken, @QueryMap Map<String, String> queryParams);
 }
